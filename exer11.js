ccdad4d8f4e8ab5ba7c8182b6fe90f2f47344d5b
israelmurilocadorin <37721806+israelmurilocadorin@users.noreply.github.com>
2018-04-06 19:54:03 -0300

    Add files via upload

---
+++ exer11.js
@@ -0,0 +1,10 @@
+var altura, base, area, perimetro;
+
+altura = Number(prompt(" Digite o valor de a: "));
+base = Number(prompt(" Digite o valor de b: "));
+
+area = (base * altura);
+perimetro = ((2 * a) + (2 * b));
+
+alert(area);
+alert(perimetro);
