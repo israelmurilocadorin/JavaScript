4a20fbc03a150f291fc1af0f37d487b3defc4a42
israelmurilocadorin <37721806+israelmurilocadorin@users.noreply.github.com>
2018-04-06 19:56:22 -0300

    Add files via upload

---
+++ exer13.js
@@ -0,0 +1,11 @@
+var x1, x2, tr;
+
+x1 = Number(prompt(" Digite o primeiro valor: "));
+x2 = Number(prompt(" Digite o segundo valor: "));
+
+tr = x1;
+x1 = x2;
+x2 = tr;
+
+alert(" O primeiro número é :"+x1);
+alert(" O segundo número é :"+x2);
