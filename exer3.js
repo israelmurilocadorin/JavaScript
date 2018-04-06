c8383f246086dbd55ce94568c2179a3c38af8573
israelmurilocadorin <37721806+israelmurilocadorin@users.noreply.github.com>
2018-04-06 19:41:24 -0300

    Add files via upload

---
+++ exer3.js
@@ -0,0 +1,9 @@
+var salarioFixo, comix, montante, salarioFinal;
+
+   salarioFixo = Number(prompt(" Digite o salario fixo: "));
+   montante = Number(prompt(" Digite o valor do montante: "));
+   
+   comix = (montante * 5) /100;
+   salarioFinal = salarioFixo + comix;
+   
+   alert(" O salario final é: "+salarioFinal);
