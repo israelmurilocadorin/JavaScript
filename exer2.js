ce54f06348470bcfbad3018c55a8f054d950c3b2
israelmurilocadorin <37721806+israelmurilocadorin@users.noreply.github.com>
2018-04-06 19:38:32 -0300

    Add files via upload

---
+++ exer2.js
@@ -0,0 +1,9 @@
+var nome, dia, mes, ano, telefone;
+   
+   nome = prompt(" Digite seu nome: ");
+   dia = Number(prompt(" Digite o dia do seu nascimento: "));
+   mes = Number(prompt(" Digite o mes do seu nascimento: "));
+   ano = Number(prompt(" Digite o ano do seu nascimento: "));
+   telefone = Number(prompt(" Digite o número do seu telefone: "));
+     
+	 console.log(" Nome: "+nome+ "\n Nascimento: "+dia+"/"+mes+"/"+ano+"\n Telefone: "+telefone);
